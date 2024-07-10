5c6c02ca93ef2f5a3b6fac8415470b284711a46a
Gru <78332542+gru2007@users.noreply.github.com>
2024-07-11 03:38:06 +1000

    Faster Hashing

--- src/Gml.Client/Helpers/SystemHelper.cs
+++ src/Gml.Client/Helpers/SystemHelper.cs
@@ -6,9 +6,10 @@ public class SystemHelper
 {
     public static string CalculateFileHash(string filePath, HashAlgorithm algorithm)
     {
-        using var fileStream = File.OpenRead(filePath);
-        var hashBytes = algorithm.ComputeHash(fileStream);
-
-        return BitConverter.ToString(hashBytes).Replace("-", "").ToLower();
+        using (var fileStream = new BufferedStream(File.OpenRead(filePath), 1200000))
+        {
+            var hashBytes = algorithm.ComputeHash(fileStream);
+            return BitConverter.ToString(hashBytes).Replace("-", "").ToLower();
+        }
     }
 }
