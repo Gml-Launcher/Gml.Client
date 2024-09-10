d1d7daf7a064794de66e409502305ec95c64e55d
GamerVII-NET <111225722+GamerVII-NET@users.noreply.github.com>
2024-09-10 17:54:29 +0300

    Add ProcessHelper to handle process module monitoring
    Introduce ProcessHelper with a StartWatch method to observe and log module changes of a given process. This implementation uses reactive extensions to handle the monitoring and provides module addition and removal logging.

---
+++ src/Gml.Client/Helpers/ProcessHelper.cs
@@ -0,0 +1,20 @@
+using System.Diagnostics;
+using System.Reactive;
+using System.Reactive.Linq;
+
+namespace Gml.Client.Helpers;
+
+public static class ProcessHelper
+{
+    private static IDisposable? _watchDisposable;
+
+    public static void StartWatch(this Process process)
+    {
+        _watchDisposable?.Dispose();
+
+        _watchDisposable = process.Modules
+            .Cast<ProcessModule>()
+            .ToObservable()
+            .Subscribe(module => Console.WriteLine($"Module added: {module}"), () => Console.WriteLine("A module was removed"));
+    }
+}
