a81b91b69a35d9edac84980c5d35326c77f65fda
Terentev A. A. <terentev.a.a@recloud.tech>
2025-03-26 19:40:33 +0300

    Add news feature with UI integration and GitHub service interface
    Introduce a "News" section in the Overview Page View using a new `NewsComponent`. Include interfaces for managing and retrieving news data (`INews`, `INewsData`, `INewsProvider`, `INewsListenerProvider`). Also, add the `IGitHubService` interface for future GitHub-related functionalities.

---
+++ src/Gml.Core.Interfaces/Integrations/INewsProvider.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using GmlCore.Interfaces.Enums;
+using GmlCore.Interfaces.News;
+
+namespace GmlCore.Interfaces.Integrations;
+
+public interface INewsProvider
+{
+    Task<IReadOnlyCollection<INewsData>> GetNews(int count = 20);
+    NewsListenerType Type { get; }
+    string Name { get; }
+    string Url { get; set; }
+}
