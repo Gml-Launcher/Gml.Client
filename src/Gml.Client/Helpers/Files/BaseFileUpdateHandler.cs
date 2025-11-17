4a9ff0a6f9c05b9450300c0b95e982e7644f4263
gamervii <terentev.a.a@recloud.tech>
2025-11-17 19:20:34 +0300

    Update file path construction to include "shared data" directory

--- src/Gml.Client/Helpers/Files/BaseFileUpdateHandler.cs
+++ src/Gml.Client/Helpers/Files/BaseFileUpdateHandler.cs
@@ -27,12 +27,12 @@ public class BaseFileUpdateHandler : IFileUpdateHandler
             ? Directory.GetFiles(profilePath, "*.*", SearchOption.AllDirectories)
             : [];
 
-        var assetsPath = Path.Combine(rootDirectory, "assets");
+        var assetsPath = Path.Combine(rootDirectory, "shared data", "assets");
         var assetsFiles = Directory.Exists(assetsPath)
             ? Directory.GetFiles(assetsPath, "*.*", SearchOption.AllDirectories)
             : [];
 
-        var runtimePath = Path.Combine(rootDirectory, "runtime");
+        var runtimePath = Path.Combine(rootDirectory, "shared data", "runtime");
         var runtimeFiles = Directory.Exists(runtimePath)
             ? Directory.GetFiles(runtimePath, "*.*", SearchOption.AllDirectories)
             : [];
